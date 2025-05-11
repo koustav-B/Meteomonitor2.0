7ae4ba9aef83267f63d7d397900561d44a0bfe00
Koustav Biswas <bkoustav361@gmail.com>
2025-05-12 01:20:47 +0530

    first commit

--- weather/models.py
+++ weather/models.py
@@ -1,26 +1,54 @@
 from django.db import models
 
 class WeatherData(models.Model):
-    city = models.CharField(max_length=100)
-    region = models.CharField(max_length=100, default="Unknown")  # Set default value
+    city = models.CharField(max_length=100, default="Unknown")
+    region = models.CharField(max_length=100, default="Unknown")
     country = models.CharField(max_length=100, default="Unknown")
-    local_time = models.CharField(max_length=100, default="Unknown")  # Set default value
-    temperature_c = models.FloatField(default=0.0)  # Set default value
-    feelslike_c = models.FloatField(default=0.0)  # Set default value
-    condition_text = models.CharField(max_length=100, default="Unknown")  # Set default value
-    condition_icon = models.URLField(default="")  # Set default value
+    local_time = models.CharField(max_length=100, default="Unknown")
 
-    wind_kph = models.FloatField(default=0.0)  # Set default value
-    wind_dir = models.CharField(max_length=10, default="Unknown")  # Set default value
-    pressure_mb = models.FloatField(default=0.0)  # Set default value
-    precip_mm = models.FloatField(default=0.0)  # Set default value
-    humidity = models.IntegerField(default=0)  # Set default value
-    cloud = models.IntegerField(default=0)  # Set default value
+    temperature_c = models.FloatField(default=0.0)
+    temp_f = models.FloatField(default=0.0)
+    is_day = models.BooleanField(default=True)
 
-    uv = models.FloatField(default=0.0)  # Set default value
-    vis_km = models.FloatField(default=0.0)  # Set default value
-    gust_kph = models.FloatField(default=0.0)  # Set default value
-    last_updated = models.DateTimeField(default=None, null=True)  # Set default value for DateTimeField
+    condition_text = models.CharField(max_length=100, default="Unknown")
+    condition_icon = models.URLField(default="")
+    condition_code = models.IntegerField(default=0)
+
+    wind_mph = models.FloatField(default=0.0)
+    wind_kph = models.FloatField(default=0.0)
+    wind_degree = models.IntegerField(default=0)
+    wind_dir = models.CharField(max_length=10, default="Unknown")
+
+    pressure_mb = models.FloatField(default=0.0)
+    pressure_in = models.FloatField(default=0.0)
+
+    precip_mm = models.FloatField(default=0.0)
+    precip_in = models.FloatField(default=0.0)
+
+    humidity = models.IntegerField(default=0)
+    cloud = models.IntegerField(default=0)
+
+    feelslike_c = models.FloatField(default=0.0)
+    feelslike_f = models.FloatField(default=0.0)
+
+    windchill_c = models.FloatField(default=0.0)
+    windchill_f = models.FloatField(default=0.0)
+
+    heatindex_c = models.FloatField(default=0.0)
+    heatindex_f = models.FloatField(default=0.0)
+
+    dewpoint_c = models.FloatField(default=0.0)
+    dewpoint_f = models.FloatField(default=0.0)
+
+    vis_km = models.FloatField(default=0.0)
+    vis_miles = models.FloatField(default=0.0)
+
+    uv = models.FloatField(default=0.0)
+
+    gust_mph = models.FloatField(default=0.0)
+    gust_kph = models.FloatField(default=0.0)
+
+    last_updated = models.DateTimeField(default=None, null=True)
 
     def __str__(self):
         return f"{self.city} Weather at {self.last_updated}"
